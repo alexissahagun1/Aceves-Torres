7f12b09528e392ed14b06f540f69ebc319ae399e
Alexis Sahagun <alexissahagunitj7a@gmail.com>
2023-07-16 23:54:47 -0600

    responsive design for servicios page ready

--- components/ServiciosComponent.jsx
+++ components/ServiciosComponent.jsx
@@ -6,23 +6,23 @@ const ServiciosComponent = () => {
     return (
       <div className="flex flex-col items-center mt-20 " >
            {Servicios && Servicios.map((item) =>
-                <div className='bg-gray-200 md:w-[1029px] text-center w-full flex flex-col  lg:mb-[120px]' key={item.id}>
+                <div className='bg-gray-200 xl:w-[1029px] text-center w-full flex flex-col lg:mb-[120px] mb-[100px]' key={item.id}>
                     <div className="">
                         <div className="flex flex-col mt-[50px] lg:text-left">
-                            <div className="md:mx-24 space-y-6 fontcolor">
-                                <p className="font-medium text-left xl:text-[28px] lg:text-[28px] text-[14px] leading-[36px]">{item.heading}</p>
-                                <p className="font-base text-left xl:text-[16px] lg:text-[16px] text-[14px] leading-[36px] ">{item.body}</p>
-                                <p className="font-medium text-left xl:text-[20px] lg:text-[18px] text-[14px] leading-[36px] ">{item.subheading}</p>
+                            <div className="md:mx-24 mx-4 md:space-y-6 space-y-4 fontcolor">
+                                <p className="font-medium text-left xl:text-[28px] lg:text-[28px] text-[20px] leading-[36px]">{item.heading}</p>
+                                <p className="font-base text-left xl:text-[16px] lg:text-[16px] text-[14px] leading-[36px] md:mx-0 mx-2 ">{item.body}</p>
+                                <p className="font-medium text-left xl:text-[20px] lg:text-[18px] text-[18px] leading-[36px] md:mx-0 mx-2">{item.subheading}</p>
                             </div>
-                            <ul className='md:mt-4 md:ml-36'>
+                            <ul className='md:mt-4 mt-4 md:ml-36 md:mx-0 mx-10 space-y-2 md:space-y-0'>
                                 {/* <li className="whitespace-pre-line  font-base text-left md:w-[533px]  xl:text-[20px] lg:text-[18px] text-[14px] leading-[36px] text-gray-700">{item.data}</li> */}
 
                                 {item.data.split('\n').map((i, index) => (
-                                    <li key={index} className='list-disc text-[16px] font-semibold  text-left md:w-[550px] fontcolor'>{i}</li>
+                                    <li key={index} className='list-disc text-[16px] font-semibold text-left md:w-[550px] fontcolor'>{i}</li>
                                 ))}
                             </ul>
                             <div className="flex justify-end ">
-                                <img src={item.logo} className="md:-mb-[49px] md:mr-[69px]" alt="" />
+                                <img src={item.logo} className="-mb-[49px]  md:mr-[69px] mr-10 lg:w-auto lg:h-auto w-1/3 h-1/3 " alt="" />
                             </div>
 
                         </div>
--- pages/Servicios.jsx
+++ pages/Servicios.jsx
@@ -12,7 +12,7 @@ const Servicios = () => {
     return (
         <div>
             <Title/>
-            <div className="relative md:mt-[150px] md:mx-[115px]">
+            <div className="relative md:mt-[150px] mt-32 md:mx-[115px]">
                 <Image
                 src="/FondoServicios.png"
                 alt="My Image"
@@ -20,14 +20,14 @@ const Servicios = () => {
                 width={1369}
                 height={487}
                 />
-                <div className="absolute top-0 fontcolor flex flex-col md:mx-[126px] md:mt-[80px] space-y-4">
+                <div className="absolute top-0 fontcolor flex flex-col md:mx-[126px] mx-4 md:mt-[80px] mt-2 space-y-4">
                     <h3 className="font-bold text-[48px]">-Servicios</h3>
                     <p className="ml-6">Se describen algunos de ellos</p>
                 </div>
 
 
                 
-                <div className='flex justify-center md:-mt-[270px]'>
+                <div className='flex justify-center 2xl:-mt-[260px] '>
                     <Image
                     src="/FotoServicios1.png"
                     alt="My Image"
@@ -37,7 +37,7 @@ const Servicios = () => {
                 </div>
             </div>
 
-            <div className="flex justify-center md:mt-[69px]">
+            <div className="flex justify-center md:mt-[69px] mt-10">
                 <Image
                     src="/LineaServicios.png"
                     alt="My Image"
@@ -60,89 +60,8 @@ const Servicios = () => {
 
           
 
-            {/* <div id="/Soluciones#Industrias" className="text-center flex flex-col items-center xl:mx-[420px] space-y-5 mx-8">
-                <div className="flex flex-row items-center">
-                    <img src="LineThroughHeading.png" alt="" className="mx-8 hidden md:block"/>
-                    <h2 className="uppercase stella-orange font-semibold md:text-[24px] text-lg">Beneficios para su industria o negocio</h2>
-                    <img src="LineThroughHeading.png" alt="" className="mx-8 hidden md:block"/>
-                </div>
-                <p className="font-medium text-gray-600 md:text-[18px] text-base leading-10">Conozca algunos de los beneficios que tendrá por trabajar con paneles solares.</p>
-            </div>
 
-            <Beneficios file={BeneficiosText} headingColor="stella-orange" isUppercase={false} isHidden={true} marginQuantity={"2xl:mx-[330px] xl:mx-[150px]"}/>
-
-
-                <div className="text-center flex flex-col items-center mx-4 md:mx-0 md:mt-[90px] mt-[85px]">
-                    <div className="flex flex-row items-center">
-                        <img src="LineThroughHeading.png" alt="" className="mx-8 hidden md:block"/>
-                        <h2 className="uppercase stella-orange font-semibold md:text-[24px] text-lg">Proyectos destacados</h2>
-                        <img src="LineThroughHeading.png" alt="" className="mx-8 hidden md:block"/>
-                    </div>
-                </div>
-
-                <CarouselComponent height="md:h-[400px]"/>
-
-                
-                <div className="text-center flex flex-col items-center mx-4 md:mx-0 md:mt-[90px] mt-[85px]">
-                    <div className="flex flex-row items-center md:space-x-20 space-x-4">
-                        <div className="flex flex-col items-center">
-                            <h2 className='font-semibold md:text-6xl text-sm stella-orange'>+15000</h2>
-                            <h2 className='font-medium md:text-lg text-xs'>Paneles Instalados</h2>
-                        </div>
-                        <div className="flex flex-col items-center">
-                            <h2 className='font-semibold md:text-6xl text-sm stella-orange'>+10</h2>
-                            <h2 className='font-medium md:text-lg text-xs'>Años de experiencia</h2>
-                        </div>
-                        <div className="flex flex-col items-center">
-                            <h2 className='font-semibold md:text-6xl text-sm stella-orange'>+156 kWh</h2>
-                            <h2 className='font-medium md:text-lg text-xs'>Generados con energía solar</h2>
-                        </div>
-                        <div className="flex flex-col items-center">
-                            <h2 className='font-semibold md:text-6xl text-sm stella-orange'>+100</h2>
-                            <h2 className='font-medium md:text-lg text-xs'>Empresas han confiado en nosotros</h2>
-                        </div>
-                    </div>
-                </div>
-                
-
-
-
-                <div className="text-center flex flex-col items-center mx-4 md:mx-0 md:mt-[90px] mt-[65px]">
-                    <div className="flex flex-row items-center">
-                        <img src="LineThroughHeading.png" alt="" className="mx-8 hidden md:block"/>
-                        <h2 className="uppercase stella-orange font-semibold md:text-[24px] text-lg">Nuestros Clientes</h2>
-                        <img src="LineThroughHeading.png" alt="" className="mx-8 hidden md:block"/>
-                    </div>
-                </div>
-
-                <IconsCarousel file={carouselIcons} numberItems={5} isAutoPlay={true} bgColor=""/>
-
-                <div className="text-center flex flex-col items-center mx-4 md:mx-0 md:mt-[90px] mt-[85px]">
-                    <div className="flex flex-row items-center">
-                        <img src="LineThroughHeading.png" alt="" className="mx-8 hidden md:block"/>
-                        <h2 className="uppercase stella-orange font-semibold md:text-[24px] text-lg">Alianzas</h2>
-                        <img src="LineThroughHeading.png" alt="" className="mx-8 hidden md:block"/>
-                    </div>
-                </div>
-
-                <IconsCarousel file={alianzasIcons} numberItems={3} isAutoPlay={true}/>
-
-                <Certificaciones/>
-
-                <div className="text-center flex flex-col items-center mx-4 md:mx-0 md:mt-[90px] mt-[85px]">
-                    <div className="flex flex-row items-center">
-                        <img src="LineThroughHeading.png" alt="" className="mx-8 hidden md:block"/>
-                        <h2 className="uppercase stella-orange font-semibold md:text-[24px] text-lg">Socios Comerciales</h2>
-                        <img src="LineThroughHeading.png" alt="" className="mx-8 hidden md:block"/>
-                    </div>
-                </div>
-
-                <IconsCarousel file={SociosIcons} numberItems={4} isAutoPlay={true} bgColor="bg-white"/>
-
-
-                <VideoComponent video="bgVideo.mp4" size="md:h-[660px] h-[300px]"/> */}
-
-                <div className="mt-[300px]"></div>
+                <div className="md:mt-[300px]"></div>
 
 
 
